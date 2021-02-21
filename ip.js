8a1a9f05fbb325aab6c9f363da5e681017edc1a8
Khushboo Priya <khushboopriyaiiitnr@gmail.com>
2021-02-21 16:18:04 +0530

    first commit

---
+++ ip.js
@@ -0,0 +1,15 @@
+ip = document.querySelector('#ipaddress');
+
+const url = 'https://api.ipify.org/?format=json';
+
+// fetch(url)
+// .then((response)=>response.json())
+// .then((json)=>{console.log(json)});
+
+const promise = fetch(url);
+
+promise
+.then((response)=>response.json())
+    .then((json_response)=>{
+        ip.value = json_response.ip;
+});
